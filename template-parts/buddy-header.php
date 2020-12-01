b5ed40b95714f6b6af4c4c40eb32211dfb518737
Tony Blacker <tblacker7@gmail.com>
2020-12-01 11:21:06 +0000

    added filters to top and footer navs to check active components

--- template-parts/buddy-header.php
+++ template-parts/buddy-header.php
@@ -1,11 +1,20 @@
 <?php
-$notification_link = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
-$message_link      = trailingslashit( bp_loggedin_user_domain() . bp_get_messages_slug() );
-$notifications     = bp_notifications_get_unread_notification_count( bp_loggedin_user_id() );
-//$unread_notification_count = ! empty( $notifications ) ? $notifications : 0;
-//$unread_message_count = messages_get_unread_count();
-$unread_notification_count = 3;
-$unread_message_count      = 2;
+if ( in_array( 'messages', array_keys( buddypress()->loaded_components ) ) ) {
+	$message_link = trailingslashit( bp_loggedin_user_domain() . bp_get_messages_slug() );
+	$unread_message_count = messages_get_unread_count();
+	$messages_component = 'active';
+} else {
+	$messages_component = 'false';
+}
+
+if ( in_array( 'notifications', array_keys( buddypress()->loaded_components ) ) ) {
+	$notification_link = trailingslashit( bp_loggedin_user_domain() . bp_get_notifications_slug() );
+	$notifications = bp_notifications_get_unread_notification_count( bp_loggedin_user_id() );
+	$unread_notification_count = ! empty( $notifications ) ? $notifications : 0;
+	$notifications_component = 'active';
+} else {
+	$notifications_component = 'false';
+}
 ?>
 
 
@@ -13,40 +22,32 @@ $unread_message_count      = 2;
         <div class="buddy-header-search"><?php get_search_form(); ?></div>
         <div id="buddy-search" class="buddy-head-popouts">
             <a href="#" class="buddy-search">
-               <?php echo '<img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/search.svg' . '" class="bbnavmenu header-notifications"></a>'; ?>
+				<?php echo '<img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/search.svg' . '" class="bbnavmenu header-notifications"></a>'; ?>
             </a>
         </div>
-        <div id="buddy-messages" class="buddy-head-popouts">
-			<?php
-			echo '<a href="' . $message_link . '" class="buddy-messages"><img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/messages.svg' . '" class="bbnavmenu header-messages" alt="messages"></a>';
-			?>
-
-			<?php if ( $unread_message_count > 0 ): ?>
-                <span class="count"><?php echo $unread_message_count; ?></span>
-			<?php endif; ?>
-            <div class="bottom">
-                <a href="<?php echo $message_link; ?>" class="delete-all">
-					<?php _e( 'View Inbox', 'nightingale' ); ?>
-                    <i class="bb-icon-angle-right"></i>
-                </a>
-                <i></i>
-            </div>
-        </div>
-        <div id="buddy-notifications" class="buddy-head-popouts">
-			<?php
-			echo '<a href="' . $notification_link . '" class="buddy-notifications">
-			<img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/notifications.svg' . '" class="bbnavmenu header-notifications"></a>';
-			if ( $unread_notification_count > 0 ):
+		<?php if ( 'active' === $messages_component ) { ?>
+            <div id="buddy-messages" class="buddy-head-popouts">
+				<?php
+				echo '<a href="' . $message_link . '" class="buddy-messages"><img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/messages.svg' . '" class="bbnavmenu header-messages" alt="messages"></a>';
 				?>
-                <span class="count"><?php echo $unread_notification_count; ?></span>
-			<?php endif; ?>
-            <div class="bottom">
-                <a href="<?php echo $notification_link; ?>" class="delete-all">
-					<?php _e( 'View Notifications', 'nightingale' ); ?>
-                    <i class="bb-icon-angle-right"></i>
-                </a>
-                <i></i>
+
+				<?php if ( $unread_message_count > 0 ) { ?>
+                    <span class="count"><?php echo $unread_message_count; ?></span>
+				<?php } ?>
             </div>
-        </div>
+			<?php
+		}
+		if ( 'active' === $notifications_component ) {
+			?>
+            <div id="buddy-notifications" class="buddy-head-popouts">
+				<?php
+				echo '<a href="' . $notification_link . '" class="buddy-notifications">
+			<img src="' . get_stylesheet_directory_uri() . '/assets/images/svg/buddyboss/notifications.svg' . '" class="bbnavmenu header-notifications"></a>';
+				if ( $unread_notification_count > 0 ) {
+					?>
+                    <span class="count"><?php echo $unread_notification_count; ?></span>
+				<?php } ?>
+            </div>
+		<?php } ?>
     </div>
 <?php
--- template-parts/buddynav.php
+++ template-parts/buddynav.php
@@ -7,55 +7,70 @@
  * @copyright NHS Leadership Academy, Tony Blacker
  * @version   1.1 21st August 2019
  */
-$buddy_menu_item   = array();
-if ( !is_user_logged_in() ) {
-	$buddy_menu_item[] = array( 'url' => '/wp-login.php', 'title' => 'Login' );
-	$buddy_menu_item[] = array( 'url' => '/register', 'title' => 'Register' );
-	$buddy_menu_item[] = array( 'url' => '/wp-login.php?action=lostpassword', 'title' => 'Lost your password?' );
-	$url_prefix = '';
+$buddy_menu_item = array();
+if ( ! is_user_logged_in() ) {
+	$buddy_menu_item[0] = array( 'url' => '/wp-login.php', 'title' => 'Login' );
+	$buddy_menu_item[1] = array( 'url' => '/register', 'title' => 'Register' );
+	$buddy_menu_item[2] = array( 'url' => '/wp-login.php?action=lostpassword', 'title' => 'Lost your password?' );
+	$url_prefix        = '';
 } else {
 
 	$current_url = $actual_link = ( isset( $_SERVER[ 'HTTPS' ] ) && $_SERVER[ 'HTTPS' ] === 'on' ? "https" : "http" ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-	$buddy_menu_item[] = array( 'url' => 'profile/', 'title' => 'Profile' );
-	$buddy_menu_item[] = array( 'url' => 'settings/', 'title' => 'Account' );
-	$buddy_menu_item[] = array( 'url' => 'activity', 'title' => 'Timeline' );
-	$buddy_menu_item[] = array( 'url' => 'notifications/', 'title' => 'Notifications' );
-	$buddy_menu_item[] = array( 'url' => 'messages/', 'title' => 'Messages' );
-	$buddy_menu_item[] = array( 'url' => 'friends/', 'title' => 'Connections' );
-	$buddy_menu_item[] = array( 'url' => 'groups/', 'title' => 'Groups' );
-	$buddy_menu_item[] = array( 'url' => 'guides/', 'title' => 'Courses' );
-	$buddy_menu_item[] = array( 'url' => 'forums/', 'title' => 'Forums' );
-	$buddy_menu_item[] = array( 'url' => 'photos/', 'title' => 'Photos' );
-	$buddy_menu_item[] = array( 'url' => 'invites/', 'title' => 'Email Invites' );
+	foreach ( array_keys( buddypress()->loaded_components ) as $component_id ) {
+		if ( 'profile' === $component_id ) {
+			$buddy_menu_item[0] = array( 'url' => 'profile/', 'title' => 'Profile' );
+		} elseif ( 'settings' === $component_id ) {
+			$buddy_menu_item[1] = array( 'url' => 'settings/', 'title' => 'Account' );
+		} elseif ( 'activity' === $component_id ) {
+			$buddy_menu_item[2] = array( 'url' => 'activity', 'title' => 'Timeline' );
+		} elseif ( 'notifications' === $component_id ) {
+			$buddy_menu_item[3] = array( 'url' => 'notifications/', 'title' => 'Notifications' );
+		} elseif ( 'messages' === $component_id ) {
+			$buddy_menu_item[4] = array( 'url' => 'messages/', 'title' => 'Messages' );
+		} elseif ( 'friends' === $component_id ) {
+			$buddy_menu_item[5] = array( 'url' => 'friends/', 'title' => 'Connections' );
+		} elseif ( 'groups' === $component_id ) {
+			$buddy_menu_item[6] = array( 'url' => 'groups/', 'title' => 'Groups' );
+		} elseif ( 'forums' === $component_id ) {
+			$buddy_menu_item[7] = array( 'url' => 'forums/', 'title' => 'Forums' );
+		} elseif ( 'photos' === $component_id ) {
+			$buddy_menu_item[8] = array( 'url' => 'photos/', 'title' => 'Photos' );
+		} elseif ( 'invites' === $component_id ) {
+			$buddy_menu_item[9] = array( 'url' => 'invites/', 'title' => 'Email Invites' );
+		}
+	}
+	if ( in_array( 'sfwd-lms/sfwd_lms.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+		$buddy_menu_item[10] = array( 'url' => 'guides/', 'title' => 'Courses' );
+	}
 	$url_prefix = '/members/' . wp_get_current_user()->user_login . '/';
 }
 ?>
 <div class="nhsuk-full-width-container">
-	<div class="nhsuk-bordered-tabs-container-footer">
-		<div class="nhsuk-width-container">
-			<p class="nhsuk-header__navigation-title"><a class="label-navigation-buddynav">This Section</a>
-				<button class="nhsuk-header__navigation-close close-menu-buddynav">
-					<svg class="nhsuk-icon nhsuk-icon__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-						<path d="M13.41 12l5.3-5.29a1 1 0 1 0-1.42-1.42L12 10.59l-5.29-5.3a1 1 0 0 0-1.42 1.42l5.3 5.29-5.3 5.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l5.29-5.3 5.29 5.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"></path>
-					</svg>
-					<span class="nhsuk-u-visually-hidden">Close Menu</span>
-				</button>
-			</p>
-			<ul class="nhsuk-bordered-tabs buddynav-menu">
-	<?php
+    <div class="nhsuk-bordered-tabs-container-footer">
+        <div class="nhsuk-width-container">
+            <p class="nhsuk-header__navigation-title"><a class="label-navigation-buddynav">This Section</a>
+                <button class="nhsuk-header__navigation-close close-menu-buddynav">
+                    <svg class="nhsuk-icon nhsuk-icon__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                        <path d="M13.41 12l5.3-5.29a1 1 0 1 0-1.42-1.42L12 10.59l-5.29-5.3a1 1 0 0 0-1.42 1.42l5.3 5.29-5.3 5.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l5.29-5.3 5.29 5.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"></path>
+                    </svg>
+                    <span class="nhsuk-u-visually-hidden">Close Menu</span>
+                </button>
+            </p>
+            <ul class="nhsuk-bordered-tabs buddynav-menu">
+				<?php
 
-	foreach ( $buddy_menu_item as $nav_item ) {
-	if ( strpos( $current_url, $nav_item['url'] ) !== false ) {
-		$activelink = ' nhsuk-bordered-tabs-item-active';
-	} else {
-		$activelink = '';
-	}
-		echo '<li class="nhsuk-bordered-tabs-item' . $activelink . '"><a class="nhsuk-bordered-tabs-link" href="' . esc_url( $url_prefix . $nav_item['url'] ) . '">' . esc_html( $nav_item['title'] ) . '</a></li>';
+				foreach ( $buddy_menu_item as $nav_item ) {
+					if ( strpos( $current_url, $nav_item[ 'url' ] ) !== false ) {
+						$activelink = ' nhsuk-bordered-tabs-item-active';
+					} else {
+						$activelink = '';
+					}
+					echo '<li class="nhsuk-bordered-tabs-item' . $activelink . '"><a class="nhsuk-bordered-tabs-link" href="' . esc_url( $url_prefix . $nav_item[ 'url' ] ) . '">' . esc_html( $nav_item[ 'title' ] ) . '</a></li>';
 
-	}
-	// below div is a horrible hacky workaround to stop safari from jumping links all over the show on hover. As and when upstream library gets fixed, this div can come out.
-	?>
-			</ul>
-		</div>
-	</div>
+				}
+				// below div is a horrible hacky workaround to stop safari from jumping links all over the show on hover. As and when upstream library gets fixed, this div can come out.
+				?>
+            </ul>
+        </div>
+    </div>
 </div>
