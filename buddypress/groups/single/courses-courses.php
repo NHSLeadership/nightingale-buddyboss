10ec60c08066fcc86e4215ff97d79917d363add7
Tony Blacker <tblacker7@gmail.com>
2020-12-03 14:59:24 +0000

    corrected course display routine

---
+++ buddypress/groups/single/courses-courses.php
@@ -0,0 +1,67 @@
+<?php
+
+$ld_group_id =  bp_ld_sync( 'buddypress' )->helpers->getLearndashGroupId( bp_get_current_group_id() );
+
+if ( $ld_group_id ) {
+	$post_label_prefix  = 'group';
+	$meta              = learndash_get_setting( $ld_group_id );
+	$post_price_type   = ( isset( $meta[ $post_label_prefix . '_price_type' ] ) ) ? $meta[ $post_label_prefix . '_price_type' ] : '';
+	$post_price        = ( isset( $meta[ $post_label_prefix . '_price' ] ) ) ? $meta[ $post_label_prefix . '_price' ] : '';
+	// format the Course price to be proper XXX.YY no leading dollar signs or other values.
+	if ( ( 'paynow' === $post_price_type ) || ( 'subscribe' === $post_price_type ) ) {
+		if ( '' !== $post_price ) {
+			$post_price = preg_replace( '/[^0-9.]/', '', $post_price );
+			$post_price = number_format( floatval( $post_price ), 2, '.', '' );
+		}
+	}
+	if ( ! empty( $post_price ) && ! learndash_is_user_in_group( bp_loggedin_user_id(), $ld_group_id )  ) {
+		?>
+		<div class="bp-feedback error">
+			<span class="bp-icon" aria-hidden="true"></span>
+			<p><?php echo esc_html__( 'You are not allowed to access group courses. Please purchase membership and try again.', 'buddyboss' ); ?></p>
+		</div>
+		<?php
+		return;
+	}
+}
+
+global $courses_new;
+$count 		 = count( bp_ld_sync( 'buddypress' )->courses->getGroupCourses() );
+$courses_new = bp_ld_sync( 'buddypress' )->courses->getGroupCourses();
+
+if ( $count > 1 ) {
+	$view              = get_option( 'bb_theme_learndash_grid_list', 'grid' );
+	$class_grid_active = ( 'grid' === $view ) ? 'active' : '';
+	$class_list_active = ( 'list' === $view ) ? 'active' : '';
+	$class_grid_show   = ( 'grid' === $view ) ? 'grid-view bb-grid' : '';
+	$class_list_show   = ( 'list' === $view ) ? 'list-view bb-list' : '';
+	?>
+    <div class="item-body-inner">
+        <div id="bb-learndash_profile">
+            <div id="learndash-content" class="learndash-course-list">
+                <form id="bb-courses-directory-form" class="bb-courses-directory" method="get" action="">
+                    <div class="flex align-items-center bb-courses-header">
+                        <div id="courses-dir-search" class="bs-dir-search" role="search"></div>
+                    </div>
+                    <div class="grid-view bb-grid">
+                        <div id="course-dir-list" class="course-dir-list bs-dir-list">
+                            <ul id="courses-list" class="nhsuk-card-group bb-course-items <?php echo esc_attr( $class_grid_show . $class_list_show ); ?>">
+								<?php
+								foreach ( bp_ld_sync( 'buddypress' )->courses->getGroupCourses() as $post ) :
+									setup_postdata( $post );
+									bp_locate_template( 'groups/single/courses-loop.php', true, false );
+								endforeach;
+								wp_reset_postdata();
+								?>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+	<?php
+}
+if ( 1 === $count ) {
+	bp_locate_template( 'groups/single/courses-content-display.php', true, false );
+}
---
+++ buddypress/groups/single/courses/courses.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * BuddyBoss - Member Courses
+ *
+ * @since   BuddyBoss 1.2.0
+ * @version 1.0.0
+ */
+
+$filepath = locate_template(
+	array(
+		'learndash/learndash_template_script.min.js',
+		'learndash/learndash_template_script.js',
+		'learndash_template_script.min.js',
+		'learndash_template_script.js',
+	)
+);
+
+if ( ! empty( $filepath ) ) {
+	wp_enqueue_script( 'learndash_template_script_js', str_replace( ABSPATH, '/', $filepath ), array( 'jquery' ), LEARNDASH_VERSION, true );
+	$learndash_assets_loaded[ 'scripts' ][ 'learndash_template_script_js' ] = __FUNCTION__;
+} elseif ( file_exists( LEARNDASH_LMS_PLUGIN_DIR . '/templates/learndash_template_script' . ( ( defined( 'LEARNDASH_SCRIPT_DEBUG' ) && ( LEARNDASH_SCRIPT_DEBUG === true ) ) ? '' : '.min' ) . '.js' ) ) {
+	wp_enqueue_script( 'learndash_template_script_js', LEARNDASH_LMS_PLUGIN_URL . 'templates/learndash_template_script' . ( ( defined( 'LEARNDASH_SCRIPT_DEBUG' ) && ( LEARNDASH_SCRIPT_DEBUG === true ) ) ? '' : '.min' ) . '.js', array( 'jquery' ), LEARNDASH_VERSION, true );
+	$learndash_assets_loaded[ 'scripts' ][ 'learndash_template_script_js' ] = __FUNCTION__;
+
+	$data              = array();
+	$data[ 'ajaxurl' ] = admin_url( 'admin-ajax.php' );
+	$data              = array( 'json' => json_encode( $data ) );
+	wp_localize_script( 'learndash_template_script_js', 'sfwd_data', $data );
+
+}
+wp_enqueue_script( 'learndash-grid','/wp-content/plugins/learndash-course-grid/assets/css/style.css', '', '1.0');
+
+//LD_QuizPro::showModalWindow();
+add_action( 'wp_footer', array( 'LD_QuizPro', 'showModalWindow' ), 20 );
+
+$user_id            = bp_displayed_user_id();
+$atts               = apply_filters( 'bp_learndash_user_courses_atts', array() );
+$user_courses       = apply_filters( 'bp_learndash_user_courses', ld_get_mycourses( $user_id, $atts ) );
+$usermeta           = get_user_meta( $user_id, '_sfwd-quizzes', true );
+$quiz_attempts_meta = empty( $usermeta ) ? false : $usermeta;
+$quiz_attempts      = array();
+
+if ( ! empty( $quiz_attempts_meta ) ) {
+	foreach ( $quiz_attempts_meta as $quiz_attempt ) {
+		$c                            = learndash_certificate_details( $quiz_attempt[ 'quiz' ], $user_id );
+		$quiz_attempt[ 'post' ]       = get_post( $quiz_attempt[ 'quiz' ] );
+		$quiz_attempt[ 'percentage' ] = ! empty( $quiz_attempt[ 'percentage' ] ) ? $quiz_attempt[ 'percentage' ] : ( ! empty( $quiz_attempt[ 'count' ] ) ? $quiz_attempt[ 'score' ] * 100 / $quiz_attempt[ 'count' ] : 0 );
+
+		if ( $user_id == get_current_user_id() && ! empty( $c[ 'certificateLink' ] ) && ( ( isset( $quiz_attempt[ 'percentage' ] ) && $quiz_attempt[ 'percentage' ] >= $c[ 'certificate_threshold' ] * 100 ) ) ) {
+			$quiz_attempt[ 'certificate' ] = $c;
+		}
+		$quiz_attempts[ learndash_get_course_id( $quiz_attempt[ 'quiz' ] ) ][] = $quiz_attempt;
+	}
+}
+?>
+<header class="entry-header notifications-header flex">
+    <h1 class="entry-title flex-1">My Learning</h1>
+</header>
+<?php bp_get_template_part( 'members/single/parts/item-subnav' ); ?>
+<div class="learndash-wrapper">
+[ld_course_list mycourses="true" progress_bar="true" col="3" course_categoryselector="true"]
+</div>
