0488cbb66ab941024be8b9a06304ca17ef4be3d0
Andrew Blane <andrew.blane@leadershipacademy.nhs.uk>
2022-10-28 13:17:24 +0100

    [refs #DI-4411] Restrict filters to groups only To prevent empty filter lists being displayed.

--- buddypress/common/search-and-filters-bar.php
+++ buddypress/common/search-and-filters-bar.php
@@ -6,29 +6,31 @@
  * @version 3.1.0
  */
 ?>
-<div class="subnav-filters filters no-ajax nhsuk-grid-column-full" id="subnav-filters">
+<?php if ( bp_current_component() === 'groups' ) : ?>
+	<div class="subnav-filters filters no-ajax nhsuk-grid-column-full" id="subnav-filters">
 
-	<?php if ( 'friends' !== bp_current_component() ) : ?>
+		<?php if ( 'friends' !== bp_current_component() ) : ?>
 
-		<?php if ( 'members' !== bp_current_component() || bp_disable_advanced_profile_search() ) : ?>
-            <div class="nhsuk-buddy__search subnav-search">
-				<?php bp_nouveau_search_form(); ?>
+			<?php if ( 'members' !== bp_current_component() || bp_disable_advanced_profile_search() ) : ?>
+				<div class="nhsuk-buddy__search subnav-search">
+					<?php bp_nouveau_search_form(); ?>
 
-            </div>
+				</div>
+			<?php endif; ?>
+
 		<?php endif; ?>
+		<div class="nhsuk-grid-one-half">
+			<?php if ( bp_is_user() && ( ! bp_is_current_action( 'requests' ) && ! bp_is_current_action( 'mutual' ) ) ): ?>
+				<?php bp_get_template_part( 'common/filters/directory-filters' ); ?>
+			<?php endif; ?>
 
-	<?php endif; ?>
-    <div class="nhsuk-grid-one-half">
-		<?php if ( bp_is_user() && ( ! bp_is_current_action( 'requests' ) && ! bp_is_current_action( 'mutual' ) ) ): ?>
-			<?php bp_get_template_part( 'common/filters/directory-filters' ); ?>
-		<?php endif; ?>
+			<?php if ( 'members' === bp_current_component() || ( 'friends' === bp_current_component() && 'my-friends' === bp_current_action() ) ): ?>
+				<?php bp_get_template_part( 'common/filters/member-filters' ); ?>
+			<?php endif; ?>
 
-		<?php if ( 'members' === bp_current_component() || ( 'friends' === bp_current_component() && 'my-friends' === bp_current_action() ) ): ?>
-			<?php bp_get_template_part( 'common/filters/member-filters' ); ?>
-		<?php endif; ?>
-
-		<?php if ( 'groups' === bp_current_component() ): ?>
-			<?php bp_get_template_part( 'common/filters/group-filters' ); ?>
-		<?php endif; ?>
-    </div>
-</div><!-- search & filters -->
+			<?php if ( 'groups' === bp_current_component() ): ?>
+				<?php bp_get_template_part( 'common/filters/group-filters' ); ?>
+			<?php endif; ?>
+		</div>
+	</div><!-- search & filters -->
+<?php endif; ?>
